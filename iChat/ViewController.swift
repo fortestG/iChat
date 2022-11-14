7149c681c142209eb13b06ee478e256d781fd8f2
adored <khaykin2001@mail.ru>
2022-11-14 21:43:48 +0300

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Grigory Khaykin on 14.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
